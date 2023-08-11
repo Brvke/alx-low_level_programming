dce51d3caed129adfb26ee8cb1594e59eaae306e
Brvke <tayebruke@gmail.com>
2023-08-10 17:29:57 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.ho>
+
+/**
+ * main -this is entry point function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
