cdfdf12e44c179dcdff4995553fd317c362d26f8
Brvke <tayebruke@gmail.com>
2023-08-14 18:13:29 -0700

    print numbers putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int A;
+
+	for (A = 0 ; A < 10 ; A++)
+		putchar(A + 48);
+	putchar('\n');
+	return (0);
+}
