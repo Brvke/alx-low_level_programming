8010907be1bfb19c3a77493a7f0cc90e3722b579
Brvke <tayebruke@gmail.com>
2023-08-14 18:09:36 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: success 0
+*/
+int main(void)
+{
+	int A;
+
+	for (A = 0 ; A < 10 ; A++)
+		putchar(A + 48);
+	putchar('\n');
+	return (0);
+}
