927cddc760130e54fc5469147e3e9ed2e27154a5
Brvke <tayebruke@gmail.com>
2023-08-19 03:51:36 -0700

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet in lowercase ten times
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char alphabetlow10x;
+
+	for (alphabetlow10x = 'a'; alphabetlow10x <= 'z'; alphabetlow10x++)
+	_putchar(alphabetlow10x);
+	_putchar('\n');
+}
