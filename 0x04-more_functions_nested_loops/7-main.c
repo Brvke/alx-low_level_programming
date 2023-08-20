e1269cec141378190c02fa80df3480a4c9403fb8
Brvke <tayebruke@gmail.com>
2023-08-20 12:55:26 -0700

    daigonal print

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_daigonal(0);
+    print_daigonal(2);
+    print_daigonal(10);
+    print_daigonal(-4);
+    return (0);
+}
