3f0b02ae26d41fc488f15559d796c111daba4167
Brvke <tayebruke@gmail.com>
2023-08-25 06:52:41 -0700

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - appendes string src to the end of dest upto nth char
+ * @dest: first  string
+ * @src: secondstring
+ * @n: number of bytes
+ * Return: the total string value
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int l = 0;
+	int i;
+
+	for (i = 0 ;dest[i] != '\0' ; i++)
+		l++;
+
+	for (i = 0 ; i < n && *src != '\0' ; i++)
+	{
+		dest[l + i] = *src;
+		src++;
+	}
+	dest[l + i] = '\0';
+	return (dest);
+}
