447d6cb65c3f1d793353b5742575536d5166a87e
Brvke <tayebruke@gmail.com>
2023-09-06 05:55:20 -0700

    static libraries

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -23,10 +23,9 @@ char *_memset(char *s, char b, unsigned int n)
    * value that will always be
    * non-negative (zero or positive)
    */
-  unsigned int i;
+	unsigned int i;
 
-  for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
     s[i] = b;
-
-  return (s);
+	return (s);
 }
