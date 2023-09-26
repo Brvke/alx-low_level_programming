07b94640820d49558eedfdd1e1abc12db77cd85f
Brvke <tayebruke@gmail.com>
2023-09-26 13:51:32 -0700

    singly linked

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,27 +8,14 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t countnode = 1;
+	size_t size = 0;
 
-	/*return 0 as number of nodes*/
-	if (h == NULL)
-		return (0);
-
-	while (h->next != NULL)
+	while (h)
 	{
-		/*if str is NULL return len as 0 and str as (nil)*/
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
+		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		/*count number of nodes*/
-		countnode += 1;
+		size++;
 	}
 
-	/*print last node*/
-	printf("[%d] %s\n", h->len, h->str);
-
-    return (countnode);
+	return (size);
 }
