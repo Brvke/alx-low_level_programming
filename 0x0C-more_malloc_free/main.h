e6f10399c5d25c65afebc6fe037746e48a07d5dd
Brvke <tayebruke@gmail.com>
2023-09-27 09:25:18 -0700

    corrections

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <string.h>
+int _putchar(char c);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
