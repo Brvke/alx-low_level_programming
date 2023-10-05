00ede208cd639e811740a7d8918b4c94e3c6d7b7
brvke <tayebruke@gmail.com>
2023-10-05 20:32:08 +0300

    flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits to flip 
+ * @n: decimal number
+ * @m: decimal number
+ * Return: number of bits to flip to get @m from @n
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int sum = 0;
+	unsigned long int exc;
+
+	exc = n ^ m;
+
+	do {
+		sum += (exc & 1);
+		exc >>= 1;
+	}
+	while (exc > 0);
+
+	return (sum);
+}
