b41276479ff4260eeb5a224136598f87409172d7
Your Name <your.email@example.com>
2023-10-27 04:27:46 +0300

    correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,71 +1,28 @@
-#include <stdio.h>
 #include "main.h"
 
-int binary_len(const char *b);
-int _power(int base, int pow);
-
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: String of zeros and ones
- *
- * Return: Converted number.
+ * binary_to_uint -converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars
+ * Return:converted number, or 0
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	int max =binary_len(b) - 1;
-	int index, power;
-	unsigned int integer = 0;
+	int i;
+	unsigned int j;
 
-	if (max < 0)
-		return (0);
-
-	for (index = 0, power = max; power >= 0; index++, power--)
-	{
-		if (b[index] == '1')
-			integer += _power(2, power);
-	}
-
-	return (integer);
-}
-
-/**
- * binary_len - finds the length of a string of ones and zeroes
- * @b: String of ones and zeroes
- *
- * Return: Length of string, or 0 if the string contains non-binary digit.
- */
-
-int binary_len(const char *b)
-{
-	int len;
-
+	j = 0;
 	if (!b)
 		return (0);
-
-	for (len = 0; b[len]; len++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[len] != '0' && b[len] != '1')
-			return (-1);
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 	}
-
-	return (len);
-}
-
-/**
- * _power - raises a number to a power
- * @base: String of zeros and ones
- * @pow: Power to raise number to
- *
- * Return: Number raised.
- */
-
-int _power(int base, int pow)
-{
-	int x, num = 1;
-
-	for (x = 0; x < pow; x++)
-		num *= base;
-
-	return (num);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+	return (j);
 }
